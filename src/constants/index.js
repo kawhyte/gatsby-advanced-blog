f024619938c3d5782da85e78685f5a8632ca0a17
Kenny Whyte <kawhyte@gmail.com>
2019-07-21 12:59:16 -0700

    Updated Page info

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,12 +6,12 @@ exports.PORTFOLIO = 'portfolio';
 exports.RESUME = 'resume';
 
 // Site Information
-exports.NAME = 'Kenny Blog';
-exports.TITLE = 'Gatsby Advanced Blog';
+exports.NAME = 'Kenny Whyte';
+exports.TITLE = 'Kenny Whyte';
 exports.PREFIX = 'KENNY | ';
-exports.DESCRIPTION = 'Create your own blog with Gatsby Advanced Blog';
+exports.DESCRIPTION = 'Blog + Portfolio';
 exports.AUTHOR = 'kawhyte';
-exports.EMAIL = 'test@gmail.com';
+exports.EMAIL = 'kawhyte@gmail.com';
 exports.KEYWORDS = [
   'Gatsby',
   'ReactJS',
@@ -21,8 +21,8 @@ exports.PROFILE = 'https://avatars3.githubusercontent.com/u/42737121?s=200&v=4';
 
 // Social
 exports.GITHUB_ID = 'kawhyte';
-exports.TWITTER_ID = 'j1ism';
-exports.FACEBOOK_ID = 'j1.chic';
+exports.TWITTER_ID = '';
+exports.FACEBOOK_ID = '';
 exports.LINKEDIN_ID = 'kawhyte';
 
 // Disqus (Recommend you to use this)
